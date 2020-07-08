4b166e5be5478392d398dc1c25dc621515cbe3e8
Cattleya Pia <cjpia612@gmail.com>
2020-07-07 22:09:28 -0700

    Initial commits, also created DB w/ entries inserted

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Plain Cheeseburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sriracha Burger", false);
