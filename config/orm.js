d846f2684c40f00e6852338841ef6e51daacd490
Cattleya Pia <cjpia612@gmail.com>
2020-07-07 23:02:33 -0700

    Created methods to retrieve & store data

--- config/orm.js
+++ config/orm.js
@@ -37,6 +37,56 @@ const objToSql= (ob) => {
     return arr.toString();
 }
 
+// Object for all our SQL statement functions.
+const orm = {
+    selectAll: (tableInput, cb) => {
+      const queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, (err, result) => {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    insertOne: (table, cols, vals, cb) => {
+      const queryString = "INSERT INTO " + table;
+  
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals,(err, result) => {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    // An example of objColVals would be {name: panther, sleepy: true}
+    updateOne: (table, objColVals, condition, cb) => {
+      const queryString = "UPDATE " + table;
+  
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    }
+};
 
 
 module.exports = orm;
